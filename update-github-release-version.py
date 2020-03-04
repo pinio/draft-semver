cba0dbe1cdececda8f0356b4b65764224317fa82
Danilo Martins <mawkee@gmail.com>
2020-03-04 13:12:55 -0300

    Add output for version identifier

--- update-github-release-version.py
+++ update-github-release-version.py
@@ -43,10 +43,12 @@ def update_release():
     draft_release, release = get_releases(repo)
 
     current_version = semver.parse_version_info(release.title)
+    print(f"Current version: {current_version}")
     commits = list(
         repo.get_commits(sha="development", since=release.published_at)
     )
     sha_commits = {commit.sha for commit in commits}
+    print(f"Commits found: {len(sha_commits)}")
     # Expensive. We might revisit it in the future
     # https://developer.github.com/v3/repos/commits/#list-pull-requests-associated-with-commit  # noqa
     # https://github.com/PyGithub/PyGithub/issues/1414
@@ -61,19 +63,26 @@ def update_release():
         if pull_sha_commits & sha_commits:
             # This PR was merged and this code is on development branch
             pulls.append(pull)
+    print(f"Pull requests found: {len(pulls)}")
     # Check for `major` and `minor` labels
     labels = chain(*[pull.labels for pull in pulls])
     labels = {l.name for l in labels}
     if "bump-major" in labels:
         next_version = str(current_version.bump_major())
+        print("Bumping major version")
     elif "bump-minor" in labels:
         next_version = str(current_version.bump_minor())
+        print("Bumping minor version")
     else:
         next_version = str(current_version.bump_patch())
+        print("Bumping patch version")
     # Update draft request with the new name
     draft_release.update_release(
         name=next_version, message=draft_release.body, draft=True
     )
+    print(f"Next version: {next_version}")
+    # https://help.github.com/en/actions/reference/development-tools-for-github-actions#set-an-output-parameter-set-output  # noqa
+    print(f"::set-output name=version-identifier::{next_version}")
 
 
 if __name__ == "__main__":
